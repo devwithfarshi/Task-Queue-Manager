5fd0de3e0014a74cf37e00aedd96defce5fed059
devwithfarshi <devwithfarshi@gmail.com>
2025-02-20 01:39:06 +0600

    jwt utils update

--- backend/src/utils/jwtToken.ts
+++ backend/src/utils/jwtToken.ts
@@ -1,23 +1,24 @@
-import jwt from 'jsonwebtoken'
+import jwt, { Secret } from 'jsonwebtoken'
 
-export const generateAccessToken = (user: IUser) => {
-  return jwt.sign(
-    {
-      _id: user._id,
-      email: user.email
-    },
-    process.env.ACCESS_TOKEN_SECRET!,
-    {
-      expiresIn: process.env.ACCESS_TOKEN_LIFE!
-    }
-  )
+export const generateAccessToken = (user: IUser): string => {
+  const secret = process.env.ACCESS_TOKEN_SECRET as Secret
+  const expiresIn = process.env.ACCESS_TOKEN_LIFE || '1h'
+
+  if (!secret) {
+    throw new Error('ACCESS_TOKEN_SECRET is not defined')
+  }
+
+  return jwt.sign({ _id: user._id, email: user.email }, secret, { expiresIn })
 }
 
 export const verifyAccessToken = (
   token: string
 ): Pick<IUser, '_id' | 'email'> => {
-  return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as Pick<
-    IUser,
-    '_id' | 'email'
-  >
+  const secret = process.env.ACCESS_TOKEN_SECRET as Secret
+
+  if (!secret) {
+    throw new Error('ACCESS_TOKEN_SECRET is not defined')
+  }
+
+  return jwt.verify(token, secret) as Pick<IUser, '_id' | 'email'>
 }
