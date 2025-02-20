4230393257ac13cff56a2967aac758af5c1a373e
devwithfarshi <devwithfarshi@gmail.com>
2025-02-20 16:05:26 +0600

    socket cors update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ export const userSockets = new Map<string, string>()
 
 export const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST']
   }
 })
