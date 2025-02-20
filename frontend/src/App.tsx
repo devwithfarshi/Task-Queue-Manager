c955a508fcb33989647f0d718a210268fe6c6079
devwithfarshi <devwithfarshi@gmail.com>
2025-02-20 16:02:18 +0600

    scoket URL update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -45,7 +45,7 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:5000");
+    const newSocket = io(`${import.meta.env.VITE_API_URL}`);
 
     newSocket.on("connect", () => {
       console.log(`Connected to socket server`);
